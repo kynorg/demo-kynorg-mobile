5d2d8ea4f5a450d48c512cb4f4ad2c43c1ec4275
yusuf <yuusuf4real@yahoo.com>
2021-09-20 23:59:25 +0100

    add expressjs

---
+++ mobile/server/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello KynOrg Mobile!");
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("kynorgmobile.com listening at http://%s:%s", host, port);
+});
