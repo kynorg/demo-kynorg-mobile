d00f567e0be3610a48f8e60930f67975f5b68496
Usman Abdulganiy Babatunde <ausman@aimsammi.com>
2021-09-20 10:50:03 +0100

    screen updates3

--- mobile/Screens/StackScreen.js
+++ mobile/Screens/StackScreen.js
@@ -8,8 +8,12 @@ import SignUp from './SignUp';
 const Stack = createStackNavigator();
 
 const StackScreen = ({navigation}) => (
-    <Stack.Navigator headerShown='false'>
-        <Stack.Screen name="Welcome" component={Welcome}/>
+    <Stack.Navigator screenOptions={{
+        headerStyle: {
+            backgroundColor: '#FE5900',
+        }
+    }}>
+        <Stack.Screen name="Welcome" component={Welcome} options={{title:''}}/>
         <Stack.Screen name="SignIn" component={SignIn}/>
         <Stack.Screen name="SignUp" component={SignUp}/>
     </Stack.Navigator>
