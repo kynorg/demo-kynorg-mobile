b53c4dcaff44fffd23959032e819ce1f71da2dac
shiggabyte <justmeontop@gmail.com>
2021-09-30 00:54:23 +0100

    Add eslint to server

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,7 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
 
-//database credentials-- please supply the appropriate value as set by user
+// database credentials-- please supply the appropriate value as set by user
 module.exports = new Sequelize('kynorg_mobile_db', 'postgres', 'password', {
   host: 'localhost',
   dialect: 'postgres'
-});
-
+})
--- server/server.js
+++ server/server.js
@@ -1,30 +1,25 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+app.use(morgan('combined'))
 
-const app = express();
-
-app.use(morgan('combined'));
-
-//database connection
-const db = require("./config/database");
-
-//confirm database connection
-/*db.authenticate()
-    .then(()=> console.log("Database Connected"))
+// database connection
+/* const db = require('./config/database');
+// confirm database connection
+db.authenticate()
+    .then(()=> console.log('Database Connected'))
     .catch((err) => console.error(`Error ${err}`));
 */
+// users routes
+app.use('/users', require('./routes/User'))
 
+app.get('/', (req, res) => {
+  res.send('Hello KynOrg Mobile!')
+})
 
-//users routes 
-app.use("/users", require("./routes/User"));
+const server = app.listen(3000, () => {
+  const host = server.address().address
+  const port = server.address().port
 
-app.get("/", function(req, res) {
-  res.send("Hello KynOrg Mobile!");
-});
-
-const server = app.listen(3000, function() {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log("kynorgmobile.com listening at http://%s:%s", host, port);
-});
+  console.log('kynorgmobile.com listening at http://%s:%s', host, port)
+})
