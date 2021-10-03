ff1425cb0dae506cb1f7bb098de2a3b125b8b593
shiggabyte <justmeontop@gmail.com>
2021-10-03 02:23:13 +0100

    Add eslint & prettier to server

--- server/server.js
+++ server/server.js
@@ -1,30 +1,25 @@
-const express = require("express");
-const morgan = require("morgan");
-
+const express = require('express');
+const morgan = require('morgan');
+// database connection
+/* const db =  require("./config/database"); */
 const app = express();
 
 app.use(morgan('combined'));
-
-//database connection
-const db = require("./config/database");
-
-//confirm database connection
-/*db.authenticate()
+// confirm database connection
+/* db.authenticate()
     .then(()=> console.log("Database Connected"))
     .catch((err) => console.error(`Error ${err}`));
 */
+// users routes
+app.use('/users', require('./routes/User'));
 
-
-//users routes 
-app.use("/users", require("./routes/User"));
-
-app.get("/", function(req, res) {
-  res.send("Hello KynOrg Mobile!");
+app.get('/', (req, res) => {
+  res.send('Hello KynOrg Mobile!');
 });
 
-const server = app.listen(3000, function() {
+const server = app.listen(3000, () => {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log("kynorgmobile.com listening at http://%s:%s", host, port);
+  console.log('kynorgmobile.com listening at http://%s:%s', host, port);
 });
