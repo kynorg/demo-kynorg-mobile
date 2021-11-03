16f6595cddab096ce6b5bd10da15cd26b105e9e1
shiggabyte <justmeontop@gmail.com>
2021-11-03 16:53:47 +0100

    clear welcome page error

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 module.exports = new Sequelize(
   'kynorg_mobile_db',
   'postgres',
-  '', // 'Mesculentus1100',
+  'Mesculentus1100', // 'Mesculentus1100',
   {
     host: 'localhost',
     dialect: 'postgres',
